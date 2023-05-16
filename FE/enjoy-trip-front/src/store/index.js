5ce589f3c67ff4aacc074d12accc409619872585
seongho <tjdghgk98@gmail.com>
2023-05-16 16:00:23 +0900

    init : 초기 세팅

---
+++ FE/enjoy-trip-front/src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import Vuex from "vuex";
+//import http from "@/util/http-commons.js";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
---
+++ FE/enjoy-trip-front/src/util/http-commons.js
@@ -0,0 +1,7 @@
+// 1. axios 라이브러리 불러오기
+import axios from "axios";
+
+// 2. axios 객체 생성
+export default axios.create({
+  baseURL: "http://localhost:8080",
+});
