807e73330f01220de2b0ae56a4575f4eff24df55
SeongHo Ha <tjdghgk98@gmail.com>
2023-05-17 08:07:28 +0900

    chore: 테스트를 위한 라우터링크 추가

--- FE/enjoy-trip-front/src/App.vue
+++ FE/enjoy-trip-front/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/attractions">관광지로 이동</router-link> |
+      <router-link to="/admin">회원 관리로 이동</router-link>
     </nav>
     <router-view />
   </div>
