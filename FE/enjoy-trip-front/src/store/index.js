9c550e88277b7fe1c5b52907af12ccf2d876d9df
seongho <tjdghgk98@gmail.com>
2023-05-17 11:39:01 +0900

    feat: 마커 수정중

--- FE/enjoy-trip-front/src/store/index.js
+++ FE/enjoy-trip-front/src/store/index.js
@@ -1,13 +1,35 @@
 import Vue from "vue";
 import Vuex from "vuex";
-//import http from "@/util/http-commons.js";
+import http from "@/util/http-commons.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    attractions: [],
+  },
+  getters: {
+    attractions(state) {
+      return state.attractions;
+    },
+  },
+  mutations: {
+    ATTRACTIONS(state, payload) {
+      state.attractions = payload.attractions;
+    },
+  },
+  actions: {
+    attractionSearch(context, payload) {
+      http.post("/trips", payload.attractionSearchInfo).then((response) => {
+        console.log("서버 응답이 왔습니다", response.data);
+        // console.log(payload.attractionSearchInfo);
+        //뮤테이션 호출 -> 동기화
+        context.commit({
+          type: "ATTRACTIONS",
+          attractions: response.data,
+        });
+      });
+    },
+  },
   modules: {},
 });
