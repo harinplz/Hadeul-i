d496ef4fcea04f40177eb1b25c9f83a79db70b09
Harinee68 <harin608@naver.com>
2023-05-19 00:26:45 +0900

    mod: 관리자 회원 가입 이메일 코드 추가 #21

--- FE/enjoy-trip-front/src/components/admins/AdminUserList.vue
+++ FE/enjoy-trip-front/src/components/admins/AdminUserList.vue
@@ -30,6 +30,13 @@
             v-model="name">
           </b-form-input>
         </b-input-group>
+        <br/>        
+        <b-input-group style="width: 400px" prepend="이메일">
+          <b-form-input
+            placeholder="이메일 입력 ..."
+            v-model="email">
+          </b-form-input>
+        </b-input-group>
         <br/>
         <b-input-group style="width: 400px" prepend="비밀번호">
           <b-form-input
@@ -100,6 +107,7 @@ export default {
         user: {
           id: this.id,
           name: this.name,
+          email: this.email,
           pw: this.pw,
         },
         callback: (status) => {
