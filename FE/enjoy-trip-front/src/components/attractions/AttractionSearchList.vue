b47a6ee76f30479dc8a8705fc3d7edf608a5926f
SeongHo Ha <tjdghgk98@gmail.com>
2023-05-22 02:08:37 +0900

    feat: 오버레이를 위한 이벤트 이벤트추가

--- FE/enjoy-trip-front/src/components/attractions/AttractionSearchList.vue
+++ FE/enjoy-trip-front/src/components/attractions/AttractionSearchList.vue
@@ -2,17 +2,18 @@
   <table class="table table-striped">
     <thead>
       <tr>
-        <th scope="col">관광지명</th>
-        <th scope="col">주소</th>
-        <th scope="col">위도</th>
-        <th scope="col">경도</th>
+      <th scope="col">사진</th>
+      <th scope="col">관광지명</th>
+      <th scope="col">주소</th>
+      <th scope="col">좋아요 수</th>
+      <th scope="col">지도로</th>
       </tr>
     </thead>
-    <tbody>
+    <tbody id="table-body">
       <attraction-search-row
         v-for="attraction in attractions"
         :key="attraction.no"
-        :attraction="attraction"></attraction-search-row>
+        :attraction="attraction" @moveCenterEmit2="moveCenterEmit2"></attraction-search-row>
     </tbody>
   </table>
 </template>
@@ -25,6 +26,12 @@ export default {
   components: {
     "attraction-search-row": () =>
       import("@/components/attractions/include/AttarctionSearchRow.vue"),
+  },
+  methods: {
+    moveCenterEmit2(lat, longi){
+      
+      this.$emit("moveCenter", lat, longi);
+    }
   },
 };
 </script>
