b323671941132724a1a80010d07bb78678365375
Harinee68 <harin608@naver.com>
2023-05-21 16:03:01 +0900

    feat: HeaderNavBar.vue 초기 구현 완료 #24

--- FE/enjoy-trip-front/src/components/commons/HeaderNavBar.vue
+++ FE/enjoy-trip-front/src/components/commons/HeaderNavBar.vue
@@ -3,52 +3,50 @@
     <!-- nav 끝 -->
 
     <b-navbar toggleable="lg" type="light">
-      <b-navbar-brand href="#">EnjoyTrip!</b-navbar-brand>
+      <b-navbar-brand>
+        <router-link class="logo" :to="{ name: 'home' }"> 하들이</router-link></b-navbar-brand
+      >
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">
-            <router-link :to="{ name: 'home' }">HOME</router-link>
+          <b-nav-item>
+            <router-link class="nav_item" :to="{ name: 'home' }">HOME</router-link>
           </b-nav-item>
-          <b-nav-item href="#">
-            <router-link :to="{ name: 'attractions' }">Attraction</router-link>
+          <b-nav-item>
+            <router-link class="nav_item" :to="{ name: 'attractions' }">관광지 조회</router-link>
           </b-nav-item>
-          <b-nav-item href="#">HotPlace</b-nav-item>
-          <b-nav-item href="#">
-            <router-link :to="{ name: 'communities' }"
-              >Community</router-link
+          <b-nav-item class="nav_item">여행 계획</b-nav-item>
+          <b-nav-item>
+            <router-link class="nav_item" :to="{ name: 'Hotplace' }"
+              >핫플레이스</router-link
             ></b-nav-item
           >
-          <b-nav-item href="#">Notice</b-nav-item>
+          <b-nav-item>
+            <router-link class="nav_item" :to="{ name: 'communities' }"
+              >커뮤니티</router-link
+            ></b-nav-item
+          >
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
+        <b-navbar-nav class="user ml-auto">
           <!-- 로그인 안했을 때 -->
           <div v-if="userInfo == null">
-            <div style="display: inline">로그인이 필요합니다.&nbsp;</div>
-            <div class="login-no-link" @click="showLoginModal">
-              로그인&nbsp;
-            </div>
-            <div class="login-no-link" @click="showSignupModal">
-              회원가입&nbsp;
-            </div>
+            <div style="display: inline" id="login-no">로그인이 필요합니다.&nbsp;</div>
+            <div class="login-no-link" @click="showLoginModal">로그인&nbsp;</div>
+            <div class="login-no-link" @click="showSignupModal">회원가입&nbsp;</div>
           </div>
           <!-- 관리자 로그인 했을 때 -->
           <div v-else-if="userInfo.id == 'admin'">
             <div style="display: inline">관리자&nbsp;</div>
             <div class="login-no-link" @click="logout">로그아웃&nbsp;</div>
-            <router-link :to="{ name: 'admin' }" class="login-no-link"
-                >회원관리</router-link
-              >
+            <router-link :to="{ name: 'admin' }" class="login-no-link">회원관리</router-link>
           </div>
           <!-- 일반 회원 로그인 -->
           <div v-else>
-            <div style="display: inline">
-              {{ userInfo.name }}님 안녕하세요!&nbsp;
-            </div>
+            <div style="display: inline">{{ userInfo.name }}님 안녕하세요!&nbsp;</div>
             <div class="login-no-link" @click="logout">로그아웃&nbsp;</div>
             <div class="login-no-link">마이페이지&nbsp;</div>
           </div>
@@ -61,21 +59,18 @@
             header-bg-variant="dark"
             header-text-variant="light"
             centered
-            hide-footer>
+            hide-footer
+          >
             <b-alert show variant="danger" v-if="isLoginError">
               아이디 또는 비밀번호를 확인하세요.
             </b-alert>
             <div>
               <b-input-group style="width: 240px" prepend="아이디">
-                <b-form-input placeholder="아이디 입력 ..." v-model="user.id">
-                </b-form-input>
+                <b-form-input placeholder="아이디 입력 ..." v-model="user.id"> </b-form-input>
               </b-input-group>
               <br />
               <b-input-group style="width: 240px" prepend="비밀번호">
-                <b-form-input
-                  placeholder="비밀번호 입력 ..."
-                  v-model="user.pw"
-                  :type="`password`">
+                <b-form-input placeholder="비밀번호 입력 ..." v-model="user.pw" :type="`password`">
                 </b-form-input>
               </b-input-group>
             </div>
@@ -94,29 +89,24 @@
             header-bg-variant="dark"
             header-text-variant="light"
             centered
-            hide-footer>
+            hide-footer
+          >
             <!-- 회원 등록 모달창 Body 작성 -->
             <div class="text-center" style="margin-bottom: 10px">
               <b-input-group style="width: 400px" prepend="아이디">
-                <b-form-input placeholder="아이디 입력 ..." v-model="user.id">
-                </b-form-input>
+                <b-form-input placeholder="아이디 입력 ..." v-model="user.id"> </b-form-input>
               </b-input-group>
               <br />
               <b-input-group style="width: 400px" prepend="이름">
-                <b-form-input placeholder="이름 입력 ..." v-model="user.name">
-                </b-form-input>
+                <b-form-input placeholder="이름 입력 ..." v-model="user.name"> </b-form-input>
               </b-input-group>
               <br />
               <b-input-group style="width: 400px" prepend="이메일">
-                <b-form-input placeholder="이메일 입력 ..." v-model="user.email">
-                </b-form-input>
+                <b-form-input placeholder="이메일 입력 ..." v-model="user.email"> </b-form-input>
               </b-input-group>
               <br />
               <b-input-group style="width: 400px" prepend="비밀번호">
-                <b-form-input
-                  placeholder="비밀번호 입력 ..."
-                  v-model="user.pw"
-                  :type="`password`">
+                <b-form-input placeholder="비밀번호 입력 ..." v-model="user.pw" :type="`password`">
                 </b-form-input>
               </b-input-group>
             </div>
@@ -124,9 +114,7 @@
             <!-- 회원 등록 모달 창 Footer 작성 -->
             <div class="text-center">
               <b-button-group>
-                <b-button variant="secondary" @click="signup"
-                  >회원 등록</b-button
-                >
+                <b-button variant="secondary" @click="signup">회원 등록</b-button>
               </b-button-group>
             </div>
           </b-modal>
@@ -200,7 +188,6 @@ export default {
           }
         },
       });
-
     },
     signup() {
       const payload = {
@@ -242,20 +229,44 @@ export default {
 <style scoped>
 #login-no {
   /* display: none; */
-  color: #7f7f7f;
+  color: #7c7877;
 }
+
+.nav_item {
+  text-decoration: none;
+  color: #7c7877;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 0px 7px;
+}
+
 .login-no-link {
   text-decoration: none;
-  color: #7f7f7f;
+  color: #7c7877;
+  font-weight: bold;
   display: inline;
   cursor: pointer;
 }
 #login-admin {
   display: none;
-  color: #7f7f7f;
+  color: #7c7877;
+  font-weight: bold;
 }
 #login-user {
   display: none;
-  color: #7f7f7f;
+  color: #7c7877;
+  font-weight: bold;
+}
+
+.logo {
+  font-size: 25px;
+  text-decoration: none;
+  color: #61c4df;
+  padding-left: 30px;
+}
+
+.user {
+  padding-right: 30px;
+  font-size: 18px;
 }
 </style>
