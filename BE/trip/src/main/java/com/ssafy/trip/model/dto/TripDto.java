d8892c12a2806f0c93687041d43c28efd0d392f9
seongho <tjdghgk98@gmail.com>
2023-05-23 18:03:56 +0900

    feat: attraction Map 모달 전까지 완료#34

--- BE/trip/src/main/java/com/ssafy/trip/model/dto/TripDto.java
+++ BE/trip/src/main/java/com/ssafy/trip/model/dto/TripDto.java
@@ -2,89 +2,140 @@ package com.ssafy.trip.model.dto;
 
 public class TripDto {
 	
+	private String attractionNo;
+	private String category;
+	private String attractionName;
+	private String address;
+	private String imageUrl;
 	private String sido;
-	private String contentId;
-	private String title;
-	private String latitue;
+	private String gugun;
+	private String latitude;
 	private String longitude;
-	private String addr1;
+	private String attractionType;
 	private String searchKeyWord;
-
-	public TripDto() {}
-
-	public TripDto(String sido, String contentId, String title, String latitue, String longitude, String addr1,
-			String searchKeyWord) {
-		super();
-		this.sido = sido;
-		this.contentId = contentId;
-		this.title = title;
-		this.latitue = latitue;
-		this.longitude = longitude;
-		this.addr1 = addr1;
-		this.searchKeyWord = searchKeyWord;
+	private String hit;
+	
+	
+	
+	public String getHit() {
+		return hit;
 	}
 
+	public void setHit(String hit) {
+		this.hit = hit;
+	}
 
+	public String getAttractionNo() {
+		return attractionNo;
+	}
+	
+	public void setAttractionNo(String attractionNo) {
+		this.attractionNo = attractionNo;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getAttractionName() {
+		return attractionName;
+	}
+	
+	public void setAttractionName(String attractionName) {
+		this.attractionName = attractionName;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
 	public String getSido() {
 		return sido;
 	}
-
+	
 	public void setSido(String sido) {
 		this.sido = sido;
 	}
-
-	public String getContentId() {
-		return contentId;
+	
+	public String getGugun() {
+		return gugun;
 	}
-
-	public void setContentId(String contentId) {
-		this.contentId = contentId;
+	
+	public void setGugun(String gugun) {
+		this.gugun = gugun;
 	}
-
-	public String getTitle() {
-		return title;
+	
+	public String getLatitude() {
+		return latitude;
 	}
-
-	public void setTitle(String title) {
-		this.title = title;
+	
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
 	}
-
-	public String getLatitue() {
-		return latitue;
-	}
-
-	public void setLatitue(String latitue) {
-		this.latitue = latitue;
-	}
-
+	
 	public String getLongitude() {
 		return longitude;
 	}
-
+	
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
-
-	public String getAddr1() {
-		return addr1;
+	
+	public String getAttractionType() {
+		return attractionType;
 	}
-
-	public void setAddr1(String addr1) {
-		this.addr1 = addr1;
+	
+	public void setAttractionType(String attractionType) {
+		this.attractionType = attractionType;
 	}
 	
 	public String getSearchKeyWord() {
 		return searchKeyWord;
 	}
-
+	
 	public void setSearchKeyWord(String searchKeyWord) {
 		this.searchKeyWord = searchKeyWord;
 	}
-
-	@Override
-	public String toString() {
-		return "TripDto [sido=" + sido + ", contentId=" + contentId + ", title=" + title + ", latitue=" + latitue
-				+ ", longitude=" + longitude + ", addr1=" + addr1 + ", searchKeyWord=" + searchKeyWord + "]";
+	
+	public TripDto(String attractionNo, String category, String attractionName, String address, String imageUrl,
+			String sido, String gugun, String latitude, String longitude, String attractionType, String searchKeyWord) {
+		
+		this.attractionNo = attractionNo;
+		this.category = category;
+		this.attractionName = attractionName;
+		this.address = address;
+		this.imageUrl = imageUrl;
+		this.sido = sido;
+		this.gugun = gugun;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.attractionType = attractionType;
+		this.searchKeyWord = searchKeyWord;
 	}
 	
+	public TripDto() {}
+	
+	@Override
+	public String toString() {
+		return "TripDto [attractionNo=" + attractionNo + ", category=" + category + ", attractionName=" + attractionName
+				+ ", address=" + address + ", imageUrl=" + imageUrl + ", sido=" + sido + ", gugun=" + gugun
+				+ ", latitude=" + latitude + ", longitude=" + longitude + ", attractionType=" + attractionType
+				+ ", searchKeyWord=" + searchKeyWord + "]";
+	}
 }
--- BE/trip/src/main/java/com/ssafy/trip/model/service/TripService.java
+++ BE/trip/src/main/java/com/ssafy/trip/model/service/TripService.java
@@ -5,10 +5,15 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.ssafy.trip.model.dto.TripDto;
+import com.ssafy.trip.model.dto.TripSearchCondition;
 
 
 public interface TripService {
 	
 	public List<TripDto> list(TripDto dto, int sort) throws SQLException;
+
+	public List<TripDto> attractionList(TripSearchCondition dto) throws SQLException;
+
+	public int getCount(String tableType) throws SQLException;
 
 }
