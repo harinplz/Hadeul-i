d0aa4290c422984a0b0fba9c2713cb02c421c85b
seongho <tjdghgk98@gmail.com>
2023-05-25 14:30:34 +0900

    feat: hotplace 기본 생성자 추가

--- BE/trip/src/main/java/com/ssafy/hotplace/model/dto/HotplaceLike.java
+++ BE/trip/src/main/java/com/ssafy/hotplace/model/dto/HotplaceLike.java
@@ -11,6 +11,8 @@ public class HotplaceLike {
 		this.userNo = userNo;
 	}
 	
+	public HotplaceLike() {}
+	
 	public int getHotplaceNo() {
 		return hotplaceNo;
 	}
@@ -28,8 +30,4 @@ public class HotplaceLike {
 	public String toString() {
 		return "HotplaceLike [hotplaceNo=" + hotplaceNo + ", userNo=" + userNo + "]";
 	}
-	
-	
-	
-
 }
