9ce2a48905eeabec17ff859e43dd4d359b37ddd8
seongho <tjdghgk98@gmail.com>
2023-05-25 11:52:47 +0900

    feat: 마이페이지 기능 구현 완료 #58
    삭제는 DB 수정해야해용..

--- FE/enjoy-trip-front/src/components/user/UserMyPage.vue
+++ FE/enjoy-trip-front/src/components/user/UserMyPage.vue
@@ -10,43 +10,58 @@
       <div class="memory_content">
         <div class="row memory_row">
           <div class="col-md-3"><b>여행 경로</b></div>
-          <div class="col-md-9">해운대 투어!</div>
+          <div class="col-md-9" v-if="this.userMypage.communityTitle">
+            {{ userMypage.communityTitle }}
+          </div>
+          <div class="col-md-9" v-else>
+            아직 여행 계획 등록을 하지 않았어요.
+          </div>
         </div>
         <div class="row memory_row">
           <div class="col-md-3"><b>핫플레이스</b></div>
-          <div class="col-md-9">해운대 암소갈비집</div>
+          <div class="col-md-9" v-if="this.userMypage.hotplaceTitle">
+            {{ userMypage.hotplaceTitle }}
+          </div>
+          <div class="col-md-9" v-else>
+            아직 핫플레이스에 글을 작성하지 않았어요.
+          </div>
         </div>
         <div class="row memory_row">
           <div class="col-md-3"><b>커뮤니티</b></div>
-          <div class="col-md-9">서울 맛집 추천해주세요!</div>
+          <div class="col-md-9" v-if="this.userMypage.travelRouteTitle">
+            {{ userMypage.travelRouteTitle }}
+          </div>
+          <div class="col-md-9" v-else>
+            아직 커뮤니티에 글을 작성하지 않았어요.
+          </div>
         </div>
       </div>
     </div>
     <div class="mypage_userinfo wow fadeInUp">
       <div class="memory_title">
-        <span class="highlight_pink">김싸피님의 정보</span>
+        <span class="highlight_pink">{{ userMypage.name }}님의 정보</span>
       </div>
       <div class="memory_content">
         <div class="row memory_row">
           <div class="col-md-3"><b>아이디</b></div>
-          <div class="col-md-9">ssafy123</div>
+          <div class="col-md-9">{{ userMypage.id }}</div>
         </div>
         <div class="row memory_row">
           <div class="col-md-3"><b>이름</b></div>
-          <div class="col-md-9">김싸피</div>
+          <div class="col-md-9">{{ userMypage.name }}</div>
         </div>
         <div class="row memory_row">
           <div class="col-md-3"><b>이메일</b></div>
-          <div class="col-md-9">ssafy@naver.com</div>
+          <div class="col-md-9">{{ userMypage.email }}</div>
         </div>
         <div class="row memory_row">
           <div class="col-md-3"><b>비밀번호</b></div>
-          <div class="col-md-9">1234</div>
+          <div class="col-md-9">{{ userMypage.pw }}</div>
         </div>
       </div>
       <div class="buttonDiv">
         <button class="btn modifyBtn" @click="showModalModify">수정</button>
-        <button class="btn deleteBtn">탈퇴</button>
+        <button class="btn deleteBtn" @click="deletMember">탈퇴</button>
       </div>
     </div>
 
@@ -55,19 +70,26 @@
       <!-- 수정 모달 창 Body 작성 -->
       <div class="text-center">
         <b-input-group style="width: 400px; padding: 10px" prepend="아이디">
-          <b-form-input placeholder="아이디 입력 ..."> </b-form-input>
+          <b-form-input
+            placeholder="아이디 입력 ..."
+            v-model="userMypage.id"></b-form-input>
         </b-input-group>
         <br />
         <b-input-group style="width: 400px; padding: 10px" prepend="이름">
-          <b-form-input placeholder="이름 입력 ..."> </b-form-input>
+          <b-form-input placeholder="이름 입력 ..." v-model="userMypage.name">
+          </b-form-input>
         </b-input-group>
         <br />
         <b-input-group style="width: 400px; padding: 10px" prepend="이메일">
-          <b-form-input placeholder="이메일 입력 ..."> </b-form-input>
+          <b-form-input
+            placeholder="이메일 입력 ..."
+            v-model="userMypage.email">
+          </b-form-input>
         </b-input-group>
         <br />
         <b-input-group style="width: 400px; padding: 10px" prepend="비밀번호">
-          <b-form-input placeholder="비밀번호 입력 ..."> </b-form-input>
+          <b-form-input placeholder="비밀번호 입력 ..." v-model="userMypage.pw">
+          </b-form-input>
         </b-input-group>
       </div>
 
@@ -76,8 +98,9 @@
       <!-- 수정 모달 창 Footer 작성 -->
       <div class="text-center">
         <b-button-group>
-          <b-button variant="outline-secondary">수정</b-button>
-          <b-button variant="secondary">삭제</b-button>
+          <b-button variant="outline-secondary" @click="updateMember"
+            >수정</b-button
+          >
         </b-button-group>
       </div>
     </b-modal>
@@ -85,10 +108,66 @@
 </template>
 
 <script>
+import http from "@/util/http-commons";
+
 export default {
+  data() {
+    return {
+      userMypage: {
+        name: "",
+      },
+      no: "",
+    };
+  },
+  created() {
+    const userNo = this.$route.params.userNo;
+    this.no = userNo;
+
+    http.get(`/user/mypage/onload/${userNo}`).then((response) => {
+      if (response.status == 200) {
+        this.userMypage = response.data;
+      } else {
+        this.userMypage = {};
+      }
+    });
+  },
+
   methods: {
     showModalModify() {
       this.$refs[`user-modify`].show();
+    },
+    deletMember() {
+      this.$router.push({ name: "home" });
+
+      /* //삭제는 나중에..
+      const userId = this.userMypage.id;
+      http.delete(`/user/mypage/${userId}`).then((response) => {
+        if (response.status == 200) {
+          alert("회원 삭제가 완료되었습니다.");
+        }
+      });*/
+    },
+    updateMember() {
+      const id = this.userMypage.id;
+      const pw = this.userMypage.pw;
+      const name = this.userMypage.name;
+      const email = this.userMypage.email;
+      const no = this.no;
+
+      const info = {
+        no,
+        id,
+        pw,
+        name,
+        email,
+      };
+
+      http.put(`/user/mypage/${id}`, info).then((response) => {
+        if (response.status == 200) {
+          alert("수정이 완료되었습니다.");
+          this.$refs[`user-modify`].hide(); //모달 숨김
+        }
+      });
     },
   },
 };
